6f60890adca7d514cbfeb32fc84b998992b6e542
devero11 <casavaspun@gmail.com>
2025-11-03 21:40:54 +0200

    java sucks

--- spring-boot/src/main/java/com/example/spring_boot_docker/controller/AuthController.java
+++ spring-boot/src/main/java/com/example/spring_boot_docker/controller/AuthController.java
@@ -9,16 +9,16 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-    private final AuthService userService;
+    private final AuthService authService;
 
-    public AuthController(AuthService userService) {
-        this.userService = userService;
+    public AuthController(AuthService authService) {
+        this.authService = authService;
     }
 
     @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping("/register")
     public Map<String, String> register(@RequestBody User user) {
-      String token = userService.register(user.getUsername(), user.getEmail(), user.getPassword());
+      String token = authService.register(user.getUsername(), user.getEmail(), user.getPassword());
       if (token == null) {
         throw new RuntimeException("Invalid credentials");
       }
@@ -28,7 +28,7 @@ public class AuthController {
     @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping("/login")
     public Map<String, String> login(@RequestBody User user) {
-      String token = userService.authenticate(user.getUsername(), user.getPassword());
+      String token = authService.authenticate(user.getUsername(), user.getPassword());
       if (token == null) {
         throw new RuntimeException("Invalid credentials");
       }
